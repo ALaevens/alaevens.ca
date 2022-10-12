ca2a911ad05ff648fd489c476a8fd9603ea3aa1c
Alexander Laevens <alaevens@ualberta.ca>
2022-10-12 14:31:49 -0600

    navbar resembles the linux shell

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,7 +5,8 @@ import icon from "./icon.png";
 export default function NavBar() {
     return (
         <nav>
-            <img src={icon} alt=""/>
+            {/* <img src={icon} alt=""/> */}
+            <h5>alex@web:~$</h5>
             <NavLink to="/">Home</NavLink>
             <NavLink to="/about-me">Resume</NavLink>
             <NavLink to="/portfolio">Portfolio</NavLink>
